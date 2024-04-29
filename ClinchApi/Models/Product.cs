d33a1f86ca967e776d584fa2782c119649022275
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-04-29 15:53:22 +0100

    Modified the Product model

--- ClinchApi/Models/Product.cs
+++ ClinchApi/Models/Product.cs
@@ -1,14 +1,22 @@
-﻿namespace ClinchApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinchApi.Models;
 
 public class Product
 {
     public int Id { get; set; }
+
+    [Required]
     public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
+
+    [Range(1, double.MaxValue, ErrorMessage = "Price must be greater than zero.")]
     public decimal Price { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than zero.")]
     public int Quantity { get; set; }
-    public int CategoryId { get; set; }
-    public string ImageUrl { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime LastUpdatedAt { get; set;}
+    public List<Category>? Categories { get; set; }
+    public Uri ImageUrl { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime LastUpdatedAt { get; set; } = DateTime.UtcNow;
 }
