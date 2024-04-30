e34db150e3883da8ff275a5b4726cd1c7c924ecb
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-04-30 17:30:25 +0100

    Modified the User model

--- ClinchApi/Models/User.cs
+++ ClinchApi/Models/User.cs
@@ -6,25 +6,34 @@ public class User
     public string Username { get; set; }
     public string PasswordHash { get; set; }
     public string FirstName { get; set; }
+    public string? MiddleName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
-    public string Phone { get; set; }
+    public string? Phone { get; set; }
+    public Gender? Gender { get; set; }
+    public DateOnly? DateOfBirth { get; set; }
     
     // Navigation properties for addresses
-    public int UserAddressId { get; set; }
-    public virtual Address UserAddress { get; set; }
+    public int? UserAddressId { get; set; }
+    public virtual Address? UserAddress { get; set; }
 
-    public int BillingAddressId { get; set; }
-    public virtual Address BillingAddress { get; set; }
+    public int? BillingAddressId { get; set; }
+    public virtual Address? BillingAddress { get; set; }
 
-    public int ShippingAddressId { get; set; }
-    public virtual Address ShippingAddress { get; set; }
+    public int? ShippingAddressId { get; set; }
+    public virtual Address? ShippingAddress { get; set; }
 
     public UserRole UserRole { get; set; }
-    public virtual ICollection<Order> Orders { get; set; }
+    public virtual ICollection<Order>? Orders { get; set; }
 }
 public enum UserRole
 {
     Customer,
     Admin
+}
+public enum Gender
+{
+    Male,
+    Female,
+    Other
 }
