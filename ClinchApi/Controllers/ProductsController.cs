da4deb3597799980c5f58f3e7e86ada5ecf15d09
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-03 15:38:58 +0100

    Added the get endpoint with filters

--- ClinchApi/Controllers/ProductsController.cs
+++ ClinchApi/Controllers/ProductsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ClinchApi.Data;
+using ClinchApi.Extensions;
+using ClinchApi.Models;
+using ClinchApi.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClinchApi.Controllers;
@@ -7,7 +11,45 @@ namespace ClinchApi.Controllers;
 [ApiController]
 public class ProductsController : ControllerBase
 {
+    private readonly ProductService _productService;
+
+    public ProductsController(ProductService productService)
+    {
+        _productService = productService;
+    }
+
     //Get all products
+    /// <summary>
+    /// Get a list of all products with the applied filters
+    /// </summary>
+    /// <param name="maxPrice"></param>
+    /// <param name="minPrice"></param>
+    /// <param name="lowStock"></param>
+    /// <param name="categoryId"></param>
+    /// <param name="createdAfter"></param>
+    /// <param name="createdBefore"></param>
+    /// <returns>A list of products</returns>
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetAllProducts(
+        [FromQuery] decimal? maxPrice,
+        [FromQuery] decimal? minPrice,
+        [FromQuery] int? lowStock,
+        [FromQuery] int? categoryId,
+        [FromQuery] DateTime? createdAfter,
+        [FromQuery] DateTime? createdBefore)
+    {
+        var products = await _productService.GetProducts();
+        products = ProductFilter.FilterProducts(
+            products, 
+            maxPrice, 
+            minPrice, 
+            lowStock, 
+            categoryId, 
+            createdAfter, 
+            createdBefore);
+
+        return Ok(products);
+    }
 
     //Get a product by ID
 
@@ -18,8 +60,8 @@ public class ProductsController : ControllerBase
     //Delete a product
 
     //------------------------------------------//
-    //Get products where max-price = n
-    //Get products where min-price = n
+    //Get products where max-price <= n
+    //Get products where min-price <= n
     //Get products where price between min - max
     //Get products that are low on stock
     //Get all products from a category
---
+++ ClinchApi/Extensions/ProductFilter.cs
@@ -0,0 +1,48 @@
+﻿using ClinchApi.Models;
+
+namespace ClinchApi.Extensions;
+
+public static class ProductFilter
+{
+    public static IEnumerable<Product> FilterProducts(
+        IEnumerable<Product> products,
+        decimal? maxPrice, 
+        decimal? minPrice, 
+        int? lowStock,
+        int? categoryId, 
+        DateTime? createdAfter, 
+        DateTime? createdBefore)
+    {
+        if (maxPrice.HasValue)
+        {
+            products = products.Where(p => p.Price <= maxPrice);
+        }
+
+        if (minPrice.HasValue)
+        {
+            products = products.Where(p => p.Price >= minPrice);
+        }
+
+        if (lowStock.HasValue)
+        {
+            products = products.Where(p => p.Quantity <= lowStock);
+        }
+
+        if (categoryId.HasValue)
+        {
+            products = products.Where(p => p.Categories.Any(c => c.Id == categoryId));
+        }
+
+        if (createdAfter.HasValue)
+        {
+            products = products.Where(p => p.CreatedAt > createdAfter);
+        }
+
+        if (createdBefore.HasValue)
+        {
+            products = products.Where(p => p.CreatedAt < createdBefore);
+        }
+
+        return products;
+    }
+}
