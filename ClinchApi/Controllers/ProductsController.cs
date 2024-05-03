75c9727b77a63dfccb52ff1458abc6e5d82cd9a7
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-03 12:58:29 +0100

    Outlined the endpoints to be created in products controller

--- ClinchApi/Controllers/ProductsController.cs
+++ ClinchApi/Controllers/ProductsController.cs
@@ -7,4 +7,23 @@ namespace ClinchApi.Controllers;
 [ApiController]
 public class ProductsController : ControllerBase
 {
+    //Get all products
+
+    //Get a product by ID
+
+    //Add a new product
+
+    //Update an existing product
+
+    //Delete a product
+
+    //------------------------------------------//
+    //Get products where max-price = n
+    //Get products where min-price = n
+    //Get products where price between min - max
+    //Get products that are low on stock
+    //Get all products from a category
+    //Get products created after {date}
+    //Get products created before {date}
+
 }
