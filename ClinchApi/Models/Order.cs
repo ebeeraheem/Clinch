9ee1cb27efd85b84f5b4c20b64a7cd034ec8e967
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-03 11:01:15 +0100

    Deleted IAddressService from the services interfaces

--- ClinchApi/Models/Order.cs
+++ ClinchApi/Models/Order.cs
@@ -3,7 +3,7 @@
 public class Order
 {
     public int Id { get; set; }
-    public string UserId { get; set; }
+    public string UserIdInt { get; set; }
     public virtual User User { get; set; }
     public DateTime OrderDate { get; set; } = DateTime.UtcNow;
     public decimal TotalAmount { get; set; }
