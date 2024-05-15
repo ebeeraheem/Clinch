6b6aaaf0c6406e7ae533eb9031db35d8b725dfed
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-15 16:17:04 +0100

    Added a ShoppingCart navigation property to the ShoppingCartItem model

--- ClinchApi/Models/ShoppingCartItem.cs
+++ ClinchApi/Models/ShoppingCartItem.cs
@@ -1,9 +1,14 @@
-﻿namespace ClinchApi.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace ClinchApi.Models;
 
 public class ShoppingCartItem
 {
     public int Id { get; set; }
     public int ShoppingCartId { get; set; }
+
+    [JsonIgnore]
+    public virtual ShoppingCart ShoppingCart { get; set; }
     public int ProductId { get; set; }
     public virtual Product Product { get; set; }
     public int Quantity { get; set; }
