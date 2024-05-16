dc322b042226b0de132f943196e346ddc136c237
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-16 15:01:01 +0100

    Added XML comments to the shopping carts controller

--- ClinchApi/Controllers/ShoppingCartsController.cs
+++ ClinchApi/Controllers/ShoppingCartsController.cs
@@ -16,12 +16,23 @@ public class ShoppingCartsController : ControllerBase
         _cartService = cartService;
     }
 
+    /// <summary>
+    /// Get all the items in a user's cart
+    /// </summary>
+    /// <param name="userId">ID of the user whose cart is to be retrieved</param>
+    /// <returns>A list of shopping cart items</returns>
     [HttpGet("{userId}/cart")]
     public async Task<ActionResult<List<ShoppingCartItem>>> GetCart(int userId)
     {
         return await _cartService.GetCart(userId);
     }
 
+    /// <summary>
+    /// Adds a new product to the user's shopping cart
+    /// </summary>
+    /// <param name="userId">ID of the user whose cart the product is to be added to</param>
+    /// <param name="productId">ID of the product to add to the cart</param>
+    /// <returns>The user's shopping cart</returns>
     [HttpPost("{userId}/cart/add/{productId}")]
     public async Task<ActionResult<ShoppingCart>> AddToCart(int userId, int productId)
     {
@@ -39,6 +50,12 @@ public class ShoppingCartsController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Increases the quantity of an item in the user's cart
+    /// </summary>
+    /// <param name="userId">ID of the user whose item is to be increased</param>
+    /// <param name="productId">ID of the product whose quantity is to be increased</param>
+    /// <returns>The user's shopping cart</returns>
     [HttpPost("{userId}/cart/increase/{productId}")]
     public async Task<ActionResult<ShoppingCart>> IncreaseQuantity(int userId, int productId)
     {
@@ -56,6 +73,12 @@ public class ShoppingCartsController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Decreases the quantity of an item in the user's cart
+    /// </summary>
+    /// <param name="userId">ID of the user whose item is to be decreased</param>
+    /// <param name="productId">ID of the product whose quantity is to be decreased</param>
+    /// <returns>The user's shopping cart</return
     [HttpPost("{userId}/cart/decrease/{productId}")]
     public async Task<ActionResult<ShoppingCart>> DecreaseQuantity(int userId, int productId)
     {
@@ -73,6 +96,12 @@ public class ShoppingCartsController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Removes an item from the user's cart
+    /// </summary>
+    /// <param name="userId">ID of the user whose cart the item will be removed from</param>
+    /// <param name="productId">ID of the product to be removed</param>
+    /// <returns>The user's shopping cart</returns>
     [HttpDelete("{userId}/cart/remove/{productId}")]
     public async Task<ActionResult<ShoppingCart>> RemoveFromCart(int userId, int productId)
     {
@@ -89,6 +118,11 @@ public class ShoppingCartsController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Removes all the items in a users shopping cart
+    /// </summary>
+    /// <param name="userId">ID of the user whose cart is to be cleared</param>
+    /// <returns>The user's empty cart</returns>
     [HttpDelete("{userId}/cart/clear")]
     public async Task<ActionResult<ShoppingCart>> ClearCart(int userId)
     {
