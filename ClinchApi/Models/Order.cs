f4e4704c0ecea7954de7e3642c82d2763c96ddb9
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-17 21:12:14 +0100

    Updated the Order model

--- ClinchApi/Models/Order.cs
+++ ClinchApi/Models/Order.cs
@@ -3,19 +3,19 @@
 public class Order
 {
     public int Id { get; set; }
-    public string UserIdInt { get; set; }
-    public virtual User User { get; set; }
+    public virtual User? User { get; set; }
     public DateTime OrderDate { get; set; } = DateTime.UtcNow;
     public decimal TotalAmount { get; set; }
     public string? OrderNotes { get; set; }
     public OrderStatus OrderStatus { get; set; }
     public int BillingAddressId { get; set; }
-    public virtual Address BillingAddress { get; set; }
-    public int? ShippingAddressId { get; set; }
-    public virtual Address ShippingAddress { get; set; }
+    public virtual Address? BillingAddress { get; set; }
+    public int ShippingAddressId { get; set; }
+    public virtual Address? ShippingAddress { get; set; }
     public int PaymentId { get; set; }
-    public virtual Payment Payment { get; set; }
-    public virtual ICollection<OrderItem> OrderItems { get; set; }
+    public virtual Payment? Payment { get; set; }
+    public required List<int> OrderItemIds { get; set; }
+    public virtual ICollection<OrderItem>? OrderItems { get; set; }
 }
 public enum OrderStatus
 {
