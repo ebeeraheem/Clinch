476162a6b23da5890fdba8c594a1be8c3c16165d
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-19 14:54:35 +0100

    Simplified the IfNotExists class

--- ClinchApi/Extensions/IfNotExists.cs
+++ ClinchApi/Extensions/IfNotExists.cs
@@ -6,13 +6,12 @@ public static class IfNotExists
 {
     public static void CreateDb(this IHost host)
     {
-        using (var scope = host.Services.CreateScope())
-        {
-            var services = scope.ServiceProvider;
-            var context = services.GetRequiredService<ApplicationDbContext>();
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
-            DbInitializer.Initialize(context);
-        }
+        using var scope = host.Services.CreateScope();
+        var services = scope.ServiceProvider;
+        var context = services
+            .GetRequiredService<ApplicationDbContext>();
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+        DbInitializer.Initialize(context);
     }
 }
