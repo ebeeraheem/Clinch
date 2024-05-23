cb278e11505b3b19b18ba52549478b68f1907ff7
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-23 18:19:28 +0100

    Added RoleService in Services

---
+++ ClinchApi/Services/RoleService.cs
@@ -0,0 +1,18 @@
+﻿using ClinchApi.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace ClinchApi.Services;
+
+public class RoleService
+{
+    private readonly RoleManager<IdentityRole<int>> _roleManager;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public RoleService(RoleManager<IdentityRole<int>> roleManager, UserManager<ApplicationUser> userManager)
+    {
+        _roleManager = roleManager;
+        _userManager = userManager;
+    }
+    
+
+}
