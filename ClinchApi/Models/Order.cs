f3a824890db3ae5c74f9e439ba9b7294434d661f
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-23 18:49:36 +0100

    Updated User to ApplicationUser in Order.cs

--- ClinchApi/Models/Order.cs
+++ ClinchApi/Models/Order.cs
@@ -3,7 +3,7 @@
 public class Order
 {
     public int Id { get; set; }
-    public virtual User? User { get; set; }
+    public virtual ApplicationUser? ApplicationUser { get; set; }
     public DateTime OrderDate { get; set; } = DateTime.UtcNow;
     public decimal TotalAmount { get; set; }
     public string? OrderNotes { get; set; }
