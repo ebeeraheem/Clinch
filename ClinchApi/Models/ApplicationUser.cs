720bbb68415c336cbaa31dda8fc936eafe766819
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-23 18:05:17 +0100

    Updated the User class to ApplicationUser and inherited from IdentityUser

--- ClinchApi/Models/ApplicationUser.cs
+++ ClinchApi/Models/ApplicationUser.cs
@@ -1,15 +1,13 @@
-﻿namespace ClinchApi.Models;
+﻿using Microsoft.AspNetCore.Identity;
 
-public class User
+namespace ClinchApi.Models;
+
+public class ApplicationUser : IdentityUser
 {
-    public int Id { get; set; }
-    public string Username { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
+    public Guid UserGuid { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string? MiddleName { get; set; }
     public string LastName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string? Phone { get; set; }
     public Gender? Gender { get; set; }
     public DateOnly? DateOfBirth { get; set; }
 
