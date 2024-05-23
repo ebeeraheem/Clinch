78fa384eaae0343348e2cc9d77db3eab1e24b058
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-23 18:12:39 +0100

    Changed default id from Guid from int

--- ClinchApi/Models/ApplicationUser.cs
+++ ClinchApi/Models/ApplicationUser.cs
@@ -2,7 +2,7 @@
 
 namespace ClinchApi.Models;
 
-public class ApplicationUser : IdentityUser
+public class ApplicationUser : IdentityUser<int>
 {
     public Guid UserGuid { get; set; }
     public string FirstName { get; set; } = string.Empty;
