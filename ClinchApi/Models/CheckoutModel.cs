7404be715357b906360fdf387a7e11abf075c842
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-25 13:13:54 +0100

    Added a checkout model

---
+++ ClinchApi/Models/CheckoutModel.cs
@@ -0,0 +1,22 @@
+﻿using ClinchApi.Entities;
+
+namespace ClinchApi.Models;
+
+public class CheckoutModel
+{
+    // In order to checkout, a user must have provided the following info
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public Gender? Gender { get; set; }
+    public DateOnly? DateOfBirth { get; set; }
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    public int PaymentId { get; set; }
+    public virtual Payment? Payment { get; set; }
+
+    public int BillingAddressId { get; set; }
+    public virtual Address? BillingAddress { get; set; }
+
+    public int ShippingAddressId { get; set; }
+    public virtual Address? ShippingAddress { get; set; }
+}
