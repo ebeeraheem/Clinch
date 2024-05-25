37a8fd1c69cfc9a2533333c86c574fcba651a428
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-25 15:18:28 +0100

    Added UserService

---
+++ ClinchApi/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿namespace ClinchApi.Services;
+
+public class UserService
+{
+    //Get user by id/guid
+
+    // Get all users (optional parameters)
+
+    // Update user details
+
+    // Log out
+
+    // Delete user account
+
+
+    //ENDPOINTS MANAGED BY IDENTITY
+    // - register, login, refresh token
+    // - confirm email, resend confirmation email
+    // - forgot password, reset password, change password (POST/manage/info)
+    // - get logged-in user info (GET/manage/info)
+}
