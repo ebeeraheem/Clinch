0605e18b84e21c428ad3e8dfacdf8b6bc7e1724e
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-28 18:02:58 +0100

    Add checkout controller

---
+++ ClinchApi/Controllers/CheckoutController.cs
@@ -0,0 +1,38 @@
+﻿using ClinchApi.Models;
+using ClinchApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinchApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CheckoutController : ControllerBase
+{
+    private readonly CheckoutService _checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService)
+    {
+        _checkoutService = checkoutService;
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> Checkout([FromBody] CheckoutModel checkoutModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var result = await _checkoutService.ProcessCheckoutAsync(checkoutModel);
+
+        if (!result.Success)
+        {
+            return BadRequest(result.Message);
+        }
+
+        return Ok(result);
+    }
+}
