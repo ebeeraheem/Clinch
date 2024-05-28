47734a045bdeb10208ba85994392b86bf9f64136
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-28 15:20:08 +0100

    Add ShoppingCartCreator class

---
+++ ClinchApi/Extensions/ShoppingCartCreator.cs
@@ -0,0 +1,17 @@
+﻿using ClinchApi.Entities;
+
+namespace ClinchApi.Extensions;
+
+public static class ShoppingCartCreator
+{
+    public static ShoppingCart CreateShoppingCart(int userId)
+    {
+        return new()
+        {
+            UserId = userId,
+            //ShoppingCartItemIds = new(),
+            ShoppingCartItems = [],
+            CreatedAt = DateTime.UtcNow
+        };
+    }
+}
