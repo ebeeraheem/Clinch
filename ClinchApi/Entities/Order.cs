f2b83e990b1c62da8bd8d3be4a8c240ea65e5a47
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-05-30 16:53:16 +0100

    Update order and order item

--- ClinchApi/Entities/Order.cs
+++ ClinchApi/Entities/Order.cs
@@ -3,7 +3,7 @@
 public class Order
 {
     public int Id { get; set; }
-    public string UserId {  get; set; } = string.Empty;
+    public int UserId {  get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public DateTime OrderDate { get; set; } = DateTime.UtcNow;
@@ -11,9 +11,7 @@ public class Order
     public string PaymentDetails { get; set; } = string.Empty;
     public string? OrderNotes { get; set; }
     public OrderStatus OrderStatus { get; set; }
-    //public int? BillingAddressId { get; set; }
     public string? BillingAddress { get; set; }
-    //public int ShippingAddressId { get; set; }
     public string? ShippingAddress { get; set; }
     public virtual List<OrderItem>? OrderItems { get; set; }
 }
