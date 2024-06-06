6e080f08c581d7e5928a1d5c5f00dc802f35ef26
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-06-06 15:12:42 +0100

    Optimise program.cs

--- ClinchApi/Program.cs
+++ ClinchApi/Program.cs
@@ -1,12 +1,8 @@
 using ClinchApi.Data;
 using ClinchApi.Extensions;
 using ClinchApi.Entities;
-using ClinchApi.Services;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Swashbuckle.AspNetCore.Filters;
-using System;
-using ClinchApi.Services.Interfaces;
+using ClinchApi.StartupConfigs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,46 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // For production or non-testing environment
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("EcommerceDb"))
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
     .LogTo(Console.WriteLine, LogLevel.Information));
 
-//// For testing environment (using in-memory database)
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseInMemoryDatabase("EcommerceDb"));
-
-builder.Services.AddIdentityApiEndpoints<ApplicationUser>(options =>
-    {
-        options.User.RequireUniqueEmail = true;
-        options.Password.RequiredLength = 8;
-    })
-    .AddRoles<IdentityRole<int>>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddScoped<IAddressService, AddressService>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
-builder.Services.AddScoped<ICheckoutService, CheckoutService>();
-builder.Services.AddScoped<PaymentService>();
-builder.Services.AddScoped<IRoleService, RoleService>();
-builder.Services.AddScoped<IUserService, UserService>();
-
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme()
-    {
-        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
-        Name = "Authorization",
-        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        Description = "Enter 'Bearer' followed by a space and the JWT token."
-    });
-    options.OperationFilter<SecurityRequirementsOperationFilter>();
-});
+builder.AddCustomServices();
+builder.AddAuthServices();
+builder.AddSwaggerConfig();
 
 var app = builder.Build();
 
---
+++ ClinchApi/StartupConfigs/StartupConfigurations.cs
@@ -0,0 +1,54 @@
+﻿using ClinchApi.Services.Interfaces;
+using ClinchApi.Services;
+using System.Runtime.CompilerServices;
+using Swashbuckle.AspNetCore.Filters;
+using ClinchApi.Data;
+using ClinchApi.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace ClinchApi.StartupConfigs;
+
+public static class StartupConfigurations
+{
+    public static void AddCustomServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<IAddressService, AddressService>();
+        builder.Services.AddScoped<ICategoryService, CategoryService>();
+        builder.Services.AddScoped<IProductService, ProductService>();
+        builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
+        builder.Services.AddScoped<ICheckoutService, CheckoutService>();
+        builder.Services.AddScoped<PaymentService>();
+        builder.Services.AddScoped<IRoleService, RoleService>();
+        builder.Services.AddScoped<IUserService, UserService>();
+    }
+
+    public static void AddSwaggerConfig(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme()
+            {
+                In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                Name = "Authorization",
+                Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                Description = "Enter 'Bearer' followed by a space and the JWT token."
+            });
+            options.OperationFilter<SecurityRequirementsOperationFilter>();
+        });
+    }
+
+    public static void AddAuthServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddIdentityApiEndpoints<ApplicationUser>(options =>
+        {
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequiredLength = 8;
+        })
+        .AddRoles<IdentityRole<int>>()
+        .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        builder.Services.AddEndpointsApiExplorer();
+    }
+}
