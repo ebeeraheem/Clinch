eb3e115e43dda5f6558ce0cbe76eb5298c011c5e
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-06-09 18:09:21 +0100

    Add user helper class

---
+++ ClinchApi/Extensions/UserHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace ClinchApi.Extensions;
+
+public static class UserHelper
+{
+    public static string? GetUserId(this ClaimsPrincipal user)
+    {
+        return user.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
